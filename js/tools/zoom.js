0f72c128a5b17ec88e30beb8a5d8ad7d76918f67
Chris Walker <chris@chriswalker.soy>
2018-09-08 15:32:55 +0100

    handle fullscreen zoom in both x and y, choosing the best axis to fit against

--- js/tools/zoom.js
+++ js/tools/zoom.js
@@ -27,16 +27,28 @@ var zoom = {
         }
         
         // Handles maximising the zoom to fit the canvas to the screen.
-        // TODO: Handle logic to determine if it should stretch to full-width or full-height.
-        //       Currently, it will always snap to full height, which doesn't work too well
-        //       for wide, landscape maps.
         function zoomFullHandler() {
-            var mod = window.innerHeight / window.app.globalConfig.map_height;
-        
+
+            var yMod = window.innerHeight / window.app.globalConfig.map_height;
+            var xMod = window.innerWidth / window.app.globalConfig.map_width;
+            var modAxis = xMod <= yMod ? "x" : "y";
+
+            switch (modAxis)
+            {
+                case "y":
+
+                    window.app.globalConfig.map_height = window.innerHeight;
+                    window.app.globalConfig.map_width *= yMod;
+                    break;
+                    
+                case "x":
+                    window.app.globalConfig.map_height *= xMod;
+                    window.app.globalConfig.map_width = window.innerWidth;
+                    break;
+            };
+
             window.app.globalConfig.map_pos_x = window.innerWidth / 2;
             window.app.globalConfig.map_pos_y = window.innerHeight / 2;
-            window.app.globalConfig.map_height = window.innerHeight;
-            window.app.globalConfig.map_width *= mod;
             window.app.drawMap();
         }
     }
